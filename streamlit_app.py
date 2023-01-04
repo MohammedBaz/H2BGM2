57fc01a2b9e22347fae4ecfb6b6de5a9f51f75dc
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2023-01-04 21:47:44 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,26 @@ except:
 
 if data is not None:
     st.write(data)
+################
+import json  
+import os 
+dictionary = {'type':st.secrets['type'],
+              'project_id':st.secrets['project_id'],
+              'private_key_id':st.secrets['private_key_id'],
+              'private_key':st.secrets['private_key'],
+              'client_email':st.secrets['client_email'],
+              'client_id':st.secrets['client_id'],
+              'auth_uri':st.secrets['auth_uri'],
+              'token_uri':st.secrets['token_uri'],
+              'auth_provider_x509_cert_url':st.secrets['auth_provider_x509_cert_url'],
+              'client_x509_cert_url':st.secrets['client_x509_cert_url']
+             }
+
+#https://signup.earthengine.google.com/#!/service_accounts
+PathtoKeyFile=os.path.join(os.getcwd(), "key.json")
+with open(os.path.join(os.getcwd(), "key.json"), 'w') as outfile:
+    json.dump(dictionary, outfile)
+
+import ee
+EE_CREDENTIALS = ee.ServiceAccountCredentials(st.secrets['client_email'], PathtoKeyFile)
+ee.Initialize(EE_CREDENTIALS)
