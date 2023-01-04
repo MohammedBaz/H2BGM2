36d3f0c0697e8e4e8ef7b985a7a9665aeb975e85
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2023-01-04 20:32:20 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import streamlit as st
 def get_pos(lat,lng):
     return lat,lng
 
-m = fl.Map(location=[21.437273,40.512714],zoom_start=13)
+m = fl.Map(location=[21.437273,40.512714],zoom_start=13,tiles="Stamen Terrain")
 
 #m.add_child(fl.LatLngPopup())
 
