bd9096b131c021f8a2eea6d562f295c3a044ae8c
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2023-01-04 23:34:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ map = st_folium(m, height=350, width=700)
 try:
   data = map['last_clicked']['lat'],map['last_clicked']['lng']
   PoI = ee.Geometry.Point(map['last_clicked']['lng'],map['last_clicked']['lat']) # Cast Lat and Long into required class
-  st.write(PoI)
+  #st.write(PoI)
   RoI = PoI.buffer(1e3) # Define a region of interest with a buffer zone of 1000 km around PoI.
   GetBldFtPrint(RoI)
 except:
