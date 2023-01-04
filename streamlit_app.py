bc32d68838926da97091aa02f600264cea6d1d62
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2023-01-04 22:03:17 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,18 @@ import ee
 EE_CREDENTIALS = ee.ServiceAccountCredentials(st.secrets['client_email'], PathtoKeyFile)
 ee.Initialize(EE_CREDENTIALS)
 st.write("____________________________________ Initalised______________________________________________")
+################
+def GetInformtionFromGoogleEarth(ImageCollectionName,ListofBands,Resultion,StartDate,EndDate,Latitude,Longitude):
+  PoI = ee.Geometry.Point(Longitude, Latitude) # Cast Lat and Long into required class
+  ImageCollection=ee.ImageCollection(ImageCollectionName) # get the image collecton from google earthengine
+  FilteredImageCollections = ImageCollection.select(ListofBands).filterDate(StartDate, EndDate) # apply filter(s):time and/or bands
+  results=FilteredImageCollections.getRegion(PoI, Resultion).getInfo() # get the time series of the required bands
+  resultsdf=pandas.DataFrame(results) #Cast the results getten from the above to dataframe
+  headers = resultsdf.iloc[0] # set the header of dataframe to the first line of the results
+  resultsdf = pandas.DataFrame(resultsdf.values[1:], columns=headers) # assign the results to the dataframe and use headers as columns names
+  resultsdf = resultsdf.dropna() # drops all rows with no data 
+  for band in ListofBands: # Convert the data to numeric values
+        resultsdf[band] = pandas.to_numeric(resultsdf[band], errors='coerce')
+  resultsdf['datetime'] = pandas.to_datetime(resultsdf['time'], unit='ms') # Convert the time field into a datetime.
+  resultsdf = resultsdf[['time','datetime',  *ListofBands]]
+  return resultsdf
