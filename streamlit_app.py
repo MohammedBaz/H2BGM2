b64f1d801ee7e6392479dab599813b07f832872a
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2023-01-04 20:31:12 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import streamlit as st
 def get_pos(lat,lng):
     return lat,lng
 
-m = fl.Map(location=[21.437273,40.512714])
+m = fl.Map(location=[21.437273,40.512714],zoom_start=13)
 
 #m.add_child(fl.LatLngPopup())
 
