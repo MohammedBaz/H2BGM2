a315b70c026b06e26b9fd123910335794e3c1b33
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2023-01-05 09:21:36 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ def GetBldFtPrint(RoI):
 def get_pos(lat,lng):
     return lat,lng
 
-m = fl.Map(location=[21.437273,40.512714],zoom_start=10)
+m = fl.Map(location=[21.437273,40.512714],zoom_start=10, tile='OpenStreetMap.Mapnik')
 
 m.add_child(fl.LatLngPopup())
 data=123456
