c5209f9dc02979cd2c422adfb03578b00e2bed0d
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2023-01-05 09:54:34 +0300

    Create EEBkGr.py

---
+++ EEBkGr.py
@@ -0,0 +1,22 @@
+import json  
+import os 
+import ee
+def EEAuth():
+  dictionary = {'type':st.secrets['type'],
+              'project_id':st.secrets['project_id'],
+              'private_key_id':st.secrets['private_key_id'],
+              'private_key':st.secrets['private_key'],
+              'client_email':st.secrets['client_email'],
+              'client_id':st.secrets['client_id'],
+              'auth_uri':st.secrets['auth_uri'],
+              'token_uri':st.secrets['token_uri'],
+              'auth_provider_x509_cert_url':st.secrets['auth_provider_x509_cert_url'],
+              'client_x509_cert_url':st.secrets['client_x509_cert_url']
+             }
+             #Full detail can be found by googling Google earth service accounts
+  PathtoKeyFile=os.path.join(os.getcwd(), "key.json")
+  with open(os.path.join(os.getcwd(), "key.json"), 'w') as outfile:
+    json.dump(dictionary, outfile)
+  EE_CREDENTIALS = ee.ServiceAccountCredentials(st.secrets['client_email'], PathtoKeyFile)
+  ee.Initialize(EE_CREDENTIALS)
+  st.write("AuthCompleted")
