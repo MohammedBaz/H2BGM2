28f9671dc243583bfe64b849b3bb0a26bceb3594
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2023-01-07 15:13:24 +0300

    Update 1_📈_الخصائص العمرانية.py

--- pages/1_📈_الخصائص العمرانية.py
+++ pages/1_📈_الخصائص العمرانية.py
@@ -25,6 +25,7 @@ def GetBldFtPrint(RoI):
   transparent_df.plot(ax=ax)
   transparent_df.plot()
   with col2:
+    st.header("الرجاء الضغط علي الخريطه للحصول علي الخصائص العمرانية")
     st.pyplot(fig)
   
   #fig_html = mpld3.fig_to_html(fig)
@@ -47,6 +48,7 @@ m.add_child(fl.LatLngPopup())
 data=123456
 
 with col1:
+  st.header("التوزيع العمراني")
   map = st_folium(m, height=350, width=350)
 try:
   data = map['last_clicked']['lat'],map['last_clicked']['lng']
