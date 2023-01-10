03dc6d90c5b818799e5091e60d37deb695eadf35
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2023-01-10 20:55:53 +0300

    Update repeated.py

--- pages/repeated.py
+++ pages/repeated.py
@@ -22,8 +22,7 @@ BldSA=ee.FeatureCollection('projects/sat-io/open-datasets/MSBuildings/Kingdom_of
 def GetBldFtPrint(RoI):
   filtered = BldSA.filterBounds(RoI)
   transparent_df = geemap.ee_to_geopandas(filtered)
-  fig,ax = plt.figure() 
-  transparent_df.plot(ax=ax) 
+  fig= transparent_df.plot() 
   fig_html = mpld3.fig_to_html(fig)
   with col2:
     st.write("التوزيع العمراني")
