54e0189bb14cc0b9a7467c29ed2c59c6cd3e1f65
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2023-01-11 00:32:30 +0300

    Update xx.py

--- pages/xx.py
+++ pages/xx.py
@@ -10,12 +10,12 @@ def st_pyplot(fig):
   size = fig.get_size_inches()
   width = int(size[0]) * 100+50
   height = int(size[1]) * 100+50
-  params = {‘legend.fontsize’: 30,
-  ‘figure.figsize’: (10, 10),
-  ‘axes.labelsize’: 30,
-  ‘axes.titlesize’: 30,
-  ‘xtick.labelsize’: 30,
-  ‘ytick.labelsize’: 30}
+  params = {'legend.fontsize': 30,
+  'figure.figsize': (10, 10),
+  'axes.labelsize': 30,
+  'axes.titlesize': 30,
+  'xtick.labelsize': 30,
+  'ytick.labelsize': 30}
   pylab.rcParams.update(params)
   fig_html = mpld3.fig_to_html(fig)
   components.html(fig_html, width=width, height=height)
