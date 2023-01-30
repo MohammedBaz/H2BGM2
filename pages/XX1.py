4cbc5252c03a50fc01026645c0c7e90eb6c801fa
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2023-01-30 05:52:39 +0300

    Update XX1.py

--- pages/XX1.py
+++ pages/XX1.py
@@ -3,6 +3,9 @@ import geemap
 
 start_date = '2021-01-01'
 end_date = '2022-01-01'
+PoI = ee.Geometry.Point([21.437273,40.512714])
+region = PoI.buffer(1e3)
+
 # Create a Sentinel-2 image composite
 image = geemap.dynamic_world_s2(region, start_date, end_date)
 vis_params = {'bands': ['B4', 'B3', 'B2'], 'min': 0, 'max': 3000}
