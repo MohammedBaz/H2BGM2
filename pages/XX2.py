619ba1c7bb38443287b02b6be89807881d586f8c
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2023-01-30 19:49:46 +0300

    Update XX2.py

--- pages/XX2.py
+++ pages/XX2.py
@@ -2,15 +2,17 @@ import streamlit as st
 import  ee
 import pandas
 
-
 def GetInformtionFromGoogleEarth(ListofBands,StartDate,EndDate,Latitude,Longitude):
   ImageCollectionName='COPERNICUS/S2_HARMONIZED'
   PoI = ee.Geometry.Point(Longitude, Latitude)
   ImageCollection=ee.ImageCollection(ImageCollectionName)
   FilteredImageCollections = ImageCollection.filterDate(StartDate, EndDate).filterMetadata('CLOUDY_PIXEL_PERCENTAGE',"less_than", 35)
+  FInstance=FilteredImageCollections.first()
+  
   ImageCollectionName1='GOOGLE/DYNAMICWORLD/V1'
   ImageCollection1=ee.ImageCollection(ImageCollectionName1)
   FilteredImageCollections1 = ImageCollection1.filterMetadata('system:index','equals',FInstance.get('system:index').getInfo())
+  
   resultsdf=pandas.DataFrame(FilteredImageCollections1) #Cast the results getten from the above to dataframe
   headers = resultsdf.iloc[0] # set the header of dataframe to the first line of the results
   resultsdf = pandas.DataFrame(resultsdf.values[1:], columns=headers) # assign the results to the dataframe and use headers as columns names
@@ -32,4 +34,3 @@ probabilityBands = [
   'built', 'bare', 'snow_and_ice'
 ]
 results=GetInformtionFromGoogleEarth(probabilityBands,StartDate,EndDate,Latitude,Longitude)
-st.write(results)
