24e50d3b84b4e1033a2bc39f8911de8ee69e9df5
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2023-01-30 20:44:21 +0300

    Update XX2.py

--- pages/XX2.py
+++ pages/XX2.py
@@ -24,4 +24,4 @@ probabilityBands = [
   'built', 'bare', 'snow_and_ice'
 ]
 results=GetInformtionFromGoogleEarth(probabilityBands,StartDate,EndDate,Latitude,Longitude)
-df = pandas.DataFrame(results)
+st.write(results[0].getInfo())
