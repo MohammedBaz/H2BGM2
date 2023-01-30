b7556863cadfc31c029e102d02abc2ca05cc4ab4
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2023-01-31 01:40:30 +0300

    Update XX3.py

--- pages/XX3.py
+++ pages/XX3.py
@@ -19,8 +19,8 @@ palette = [
   '#DFC35A', '#C4281B', '#A59B8F', '#B39FE1'
 ]
 st.write(type(x[1]))
-start_date = '2019-07-01'
-end_date = '2018-07-01'
+start_date = '2018-07-01'
+end_date = '2019-08-01'
 st.write(type(end_date))
 
 # Filter image collections by time
