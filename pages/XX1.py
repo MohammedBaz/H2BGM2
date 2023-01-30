4905f881994b7f6345890eeaff9410e4d9935130
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2023-01-30 05:54:58 +0300

    Update XX1.py

--- pages/XX1.py
+++ pages/XX1.py
@@ -10,3 +10,4 @@ region = PoI.buffer(1e3)
 image = geemap.dynamic_world_s2(region, start_date, end_date)
 vis_params = {'bands': ['B4', 'B3', 'B2'], 'min': 0, 'max': 3000}
 landcover = geemap.dynamic_world(region, start_date, end_date, return_type='hillshade')
+st.write(landcover)
