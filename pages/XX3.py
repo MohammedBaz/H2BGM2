0147b66b0912c2f32a9b987b9e37112afade315b
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2023-01-31 00:14:09 +0300

    Update XX3.py

--- pages/XX3.py
+++ pages/XX3.py
@@ -5,8 +5,7 @@ import streamlit as st
 import datetime
 #end_date=datetime.date.today()
 #start_date='2015-06-23'
-start_date, end_date =st.date_input('Enter the date range', value=None, min_value='2015-06-23', max_value=datetime.date.today(), key=None,
-              help=None, on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible")
+start_date, end_date =st.date_input('Enter the date range', min_value='2015-06-23', max_value=datetime.date.today(),[] )
 
 
 #start_date, end_date = st.date_input('start date  - end date :', [])
