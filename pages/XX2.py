08e6315fb57e733f4a291a1e46c6b142c459465e
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2023-01-30 20:03:01 +0300

    Update XX2.py

--- pages/XX2.py
+++ pages/XX2.py
@@ -2,6 +2,10 @@ import streamlit as st
 import  ee
 import pandas
 
+def setProperty(image):
+    dict = image.reduceRegion(ee.Reducer.mean(), feature_geometry)
+    return image.set(dict)
+
 def GetInformtionFromGoogleEarth(ListofBands,StartDate,EndDate,Latitude,Longitude):
   ImageCollectionName='COPERNICUS/S2_HARMONIZED'
   PoI = ee.Geometry.Point(Longitude, Latitude)
@@ -12,16 +16,8 @@ def GetInformtionFromGoogleEarth(ListofBands,StartDate,EndDate,Latitude,Longitud
   ImageCollectionName1='GOOGLE/DYNAMICWORLD/V1'
   ImageCollection1=ee.ImageCollection(ImageCollectionName1)
   FilteredImageCollections1 = ImageCollection1.filterMetadata('system:index','equals',FInstance.get('system:index').getInfo())
-  
-  resultsdf=pandas.DataFrame(FilteredImageCollections1) #Cast the results getten from the above to dataframe
-  headers = resultsdf.iloc[0] # set the header of dataframe to the first line of the results
-  resultsdf = pandas.DataFrame(resultsdf.values[1:], columns=headers) # assign the results to the dataframe and use headers as columns names
-  resultsdf = resultsdf.dropna() # drops all rows with no data 
-  for band in ListofBands: # Convert the data to numeric values
-        resultsdf[band] = pandas.to_numeric(resultsdf[band], errors='coerce')
-  resultsdf['datetime'] = pandas.to_datetime(resultsdf['time'], unit='ms') # Convert the time field into a datetime.
-  #resultsdf = resultsdf[['time','datetime',  *ListofBands]]
-  return resultsdf
+  withMean = collection.map(setProperty)
+  return withMean
 
 StartDate='2020-10-19'
 EndDate='2021-10-1'
